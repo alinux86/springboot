b36726816bf3c3d69b70c9559c8a9527051bac05
Alexander Mussitelli <alexander.mussitelli@gmail.com>
2024-01-27 12:54:57 +0100

    Add Controller, Repo, Service, Entity for Person. Do not forget import and check parenthesis

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,15 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Book;
+import com.example.demo.entity.Person;
 import com.example.demo.service.PersonService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("person")
 @AllArgsConstructor
 public class PersonController {
     private final PersonService personService;
+
+    @PostMapping(value = "", consumes = "application/json", produces = "application/json")
+    public Person create(@RequestBody Person person) {return personService.create(person);}
+
+
+    @GetMapping(value = "", produces = "application/json")
+    public List<Person> getAll() {
+        return personService.getAll();
+    }
+
 
 
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,11 +1,26 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Person;
+import com.example.demo.repository.PersonRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public Person create(Person person) {
+        return personRepository.save(person);
+    }
+
+    public List<Person> getAll() {
+        return personRepository.findAll();
+    }
+
 }
